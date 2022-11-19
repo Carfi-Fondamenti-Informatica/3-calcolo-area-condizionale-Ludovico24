ede9ceee05b2e3575633d5b2be774ba496d3c071
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-11-19 19:03:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
-   return 0;
+float a=0, b=0;
+int opzione=0;
+
+cout << "Inserisci 3 numeri" << endl;
+cin >> a >> b >> opzione;
+
+ switch (opzione){
+     case 0:
+         cout << (a*b)/2 << endl;
+     break;
+
+     case 1:
+         cout << a*a << endl;
+     break;
+
+     case 2:
+         cout << a*b << endl;
+     break;
+
+     default:
+         cout << "Opzione non valida" << endl;
+ }
+  
+    return 0;
 }
